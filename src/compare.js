483795dcf2c1d025e4a10305f2e0c40903036a79
jerzy <egornik62@gmail.com>
2023-10-10 10:41:06 +0300

    add some changes

--- src/compare.js
+++ src/compare.js
@@ -1,24 +1,34 @@
-import { parseData } from './parsers.js';
+import parseData from './parsers.js';
 import has from 'lodash/has.js';
 
 export const compareData = (file1, file2) => {
-  const arrData1 = parseData(file1);
-  const arrData2 = parseData(file2);
+  const Data1 = parseData(file1);
+  const Data2 = parseData(file2);
 
-  const keys = [...Object.keys(arrData1), ...Object.keys(arrData2)];
+  // console.log(typeof Data1);
+  // console.log();
+  // console.log(Data2);
+
+  const keys = [];
+  for (let key in Data1) {
+    keys.pu
+  }
+
+  // const keys = [...Object.keys(Data1), ...Object.keys(Data2)];
+  // console.log(keys);
   const uniqKeys = [...new Set(keys)].sort();
   const tab = ' ';
 
   return `{\n${uniqKeys.map((key) => {
     switch (true) {
-      case (!has(arrData2, key)):
-        return `${tab}- ${key}: ${arrData1[key]}\n`;
-      case (!has(arrData1, key)):
-        return `${tab}+ ${key}: ${arrData2[key]}\n`;
-      case (arrData1[key] !== arrData2[key]):
-        return `${tab}- ${key}: ${arrData1[key]}\n${tab}+ ${key}: ${arrData2[key]}\n`;
+      case (!has(Data2, key)):
+        return `${tab}- ${key}: ${Data1[key]}\n`;
+      case (!has(Data1, key)):
+        return `${tab}+ ${key}: ${Data2[key]}\n`;
+      case (Data1[key] !== Data2[key]):
+        return `${tab}- ${key}: ${Data1[key]}\n${tab}+ ${key}: ${Data2[key]}\n`;
       default:
-        return `${tab}${tab} ${key}: ${arrData2[key]}\n`;
+        return `${tab}${tab} ${key}: ${Data2[key]}\n`;
     }
   }).join('')}}`;
 };
--- src/parsers.js
+++ src/parsers.js
@@ -2,14 +2,13 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
-export const parseData = (filePath) => {
-    switch (path.extname(filePath)) {
-        case '.json':
-            return JSON.parse(readFileSync(filePath));
-        case '.yaml':
-            return yaml.load(readFileSync(filePath));
-        default:
-            throw new Error('Unsupported file extension!');
-    }
-};
-
+export default function parseData(filePath) {
+  switch (path.extname(filePath)) {
+    case '.json':
+      return JSON.parse(readFileSync(filePath));
+    case '.yaml':
+      return yaml.load(readFileSync(filePath));
+    default:
+      throw new Error('Unsupported file extension!');
+  }
+}
