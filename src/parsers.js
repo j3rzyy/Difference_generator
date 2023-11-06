2bb37b64fad11b3033b760c2576cc4b9764cfdc5
jerzy <egornik62@gmail.com>
2023-11-06 16:30:56 +0300

    Fixed error of extension recognition .yaml

--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,16 @@
 import { readFileSync } from 'fs';
-import path from 'path';
+import { basename, extname, dirname } from 'path';
 import yaml from 'js-yaml';
 
 export default (filePath) => {
-  switch (path.extname(filePath)) {
+  switch (extname(filePath)) {
     case '.json':
       return JSON.parse(readFileSync(filePath));
     case '.yaml':
       return yaml.load(readFileSync(filePath));
+    case '.yml':
+      return yaml.load(readFileSync(`${dirname(filePath)}/${basename(filePath, '.yml')}.yaml`));
     default:
-      throw new Error(`File extension '${path.extname(filePath)}' not supported!`);
+      throw new Error(`File extension '${extname(filePath)}' not supported!`);
   }
 };
